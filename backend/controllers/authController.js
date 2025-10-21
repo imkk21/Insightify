60ae37afc0b2873d9788a2509fb3fe0492b7db92
imkk21 <kunalkumar12350@gmail.com>
2025-10-21 13:01:54 +0530

    setup complete

---
+++ backend/controllers/authController.js
@@ -0,0 +1,13 @@
+// backend/controllers/authController.js
+
+export const registerUser = async (req, res) => {
+  res.send("Register endpoint working ✅");
+};
+
+export const loginUser = async (req, res) => {
+  res.send("Login endpoint working ✅");
+};
+
+export const googleLogin = async (req, res) => {
+  res.send("Google Login endpoint working ✅");
+};
